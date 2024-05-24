07df7ad90e51e9f328766d8f932a78472336a3a5
Hakairyoku <nikischitov@gmail.com>
2024-05-24 16:30:56 +0300

    release 0

--- sudoku.js
+++ sudoku.js
@@ -1,52 +1,21 @@
-function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
-}
-
 function solve(smth) {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
-
   let board = read(smth)
-  let randomNum = Math.floor(Math.random() * 10)
-
+  
   for (let i = 0; i < board.length; i++) {
     for (let k = 0; k < board[i].length; k++) {
       if (board[i][k] === null) {
-        board[i][k] === randomNum
+        board[i][k] = Math.floor(Math.random() * 10)
       }
     }
   }
-  return board
-}
-
-function isSolved(a) {
-
-  let board
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Возвращает булевое значение — решено это игровое поле или нет.
-   */
-}
-
-function prettyBoard(any) {
-  let board = solve(any)
-  let boardCheck = isSolved(board)
-
-  if boardCheck.includes(null) {
-    return isSolved(boardCheck)
+  if (isSolved(board).includes(null)) {
+    return solve(board)
   } else {
-    console.table(boardCheck)
+    return board
   }
-  
-
-
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
 }
+
+function prettyBoard(num) {
+    const board = solve(num)
+    console.table(board)
+  }
