b55b056ec7f76ee37d56dc66359fabdf30c151cf
Alekseevich06 <yakovlev.rma@gmail.com>
2024-05-24 12:53:00 +0300

    пытаюсь пробую не получается

--- sudoku.js
+++ sudoku.js
@@ -1,27 +1,78 @@
-function read() {
-  /**
-   * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+
+
+
+
+const fdfd = [
+[1,2,1,1,1,2,2,1,1]
+[1,2,1,1,1,2,2,1,1]
+[1,2,1,1,1,2,2,1,1]
+[1,2,1,1,1,2,2,1,1]
+[1,2,1,1,1,2,2,1,1]
+[1,2,1,1,1,2,2,1,1]
+[1,2,1,1,1,2,1,1,1]
+[1,2,1,1,1,2,1,1,1]
+[1,2,1,1,1,2,1,1,1]
+];
+
+function getNEWARR (fdfd) {
+  const newOI = []
+  for(let i = 0; i <= 8; i++) {
+    if(i===0){
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } else if (i === 1) {
+    newOI.push([fdfd[i][1],fdfd[i][1],fdfd[i][1],fdfd[i][1],fdfd[i][1],fdfd[i][1],fdfd[i][1],fdfd[i][1],fdfd[i][0]])
+  } else if (i === 2) {
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } else if (i === 3) {
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } else if (i === 4) {
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } else if (i === 5) {
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } else if (i === 6) {
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } else if (i === 7) {
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } else if (i === 8) {
+    newOI.push([fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0],fdfd[i][0]])
+  } 
+  }
+  return newOI
 }
 
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
-}
+console.log(getNEWARR(fdfd))
 
-function isSolved() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Возвращает булевое значение — решено это игровое поле или нет.
-   */
-}
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
-}
+// function isSolved(arr) {
+//     const new = []
+
+//     function () {
+//     const new = []
+//     for(let i = 0; i <= 8; i++) {
+//       if(i===0){
+//       new.push([arr[i][0],arr[i][0],arr[i][0],arr[i][0],arr[i][0],arr[i][0],arr[i][0],arr[i][0],arr[i][0]])
+//     }
+//     return new
+//     }
+//   }
+  
+  
+  
+  
+  
+  
+//   const newArr = arr.map(el, i) => el.map(item, indx) => {
+    
+//   }
+  
+  
+  
+  
+  
+  
+  
+//   const newArr = arr.map((el) => el.map((item, i, ar) => {
+//     if(item[i])
+
+// }
+// }
