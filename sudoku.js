a1044c8bb0f4026b567f0bb28f4b8f8bbc29a04c
Hakairyoku <nikischitov@gmail.com>
2024-05-24 14:04:42 +0300

    solveSolved

--- sudoku.js
+++ sudoku.js
@@ -9,6 +9,18 @@ function solve() {
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
    */
+
+  const board = read()
+  let randomNum = Math.floor(Math.random() * 10)
+
+  for (let i = 0; i < board.length; i++) {
+    for (let k = 0; k < board[i].length; k++) {
+      if (board[i][k] === null) {
+        board[i][k].replace(null, randomNum)
+      }
+    }
+  }
+  return board
 }
 
 function isSolved() {
